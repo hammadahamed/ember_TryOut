c4c63f09ee56adcc7dd909297ad67a25884ca977
hammad ahamed <hammadahamed786@gmail.com>
2021-02-17 22:46:12 +0530

    comments for js file

--- app/components/rental/image.js
+++ app/components/rental/image.js
@@ -4,7 +4,10 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object'
 
 export default class RentalImageComponent extends Component {
+    // Its just like the .obs ib GetX in flutter ...chill baby
     @tracked isLarge = false;
+
+    // and its like a .obs for methods. Probably mber.js specific things you know ;) .....
     @action toggleSize() {
         this.isLarge = !this.isLarge;
     }
